b1268ee4800c2694ddbfe353d589c5722948aec4
Tushar7102 <tp126090@gmail.com>
2025-08-28 15:01:22 +0530

    update

--- server/server.js
+++ server/server.js
@@ -44,13 +44,12 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://cosmicpowertech.com', 'https://api.cosmicpowertech.com'], // Allow specific origins
-  methods: ["GET", "POST", "PUT", "DELETE" , "PATCH"],
+  origin: ['https://cosmicpowertech.com', 'https://api.cosmicpowertech.com'], // Allow all origins
+  methods: ["GET", "POST", "PUT", "DELETE" ,"OPTIONS" , "PATCH"],
   credentials: true,
 }));
-
-app.use(express.json({ limit: '200mb' }));
-app.use(express.urlencoded({ limit: '200mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(morgan('dev'));
 
 // Serve uploaded files
