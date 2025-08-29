9dbda5ea050bd1063f01bf40b0badf932f3dae9e
Tushar7102 <tp126090@gmail.com>
2025-08-29 16:21:01 +0530

    update

--- server/server.js
+++ server/server.js
@@ -48,8 +48,8 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE" ,"OPTIONS" , "PATCH"],
   credentials: true,
 }));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ limit: '200mb', extended: true }));
 app.use(morgan('dev'));
 
 // Serve uploaded files
